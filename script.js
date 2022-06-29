c83339a41474258fcee5b6831148acc7bc0fc36b
rayssa2211 <jurcarayssaeduarda@gmail.com>
2022-06-29 20:50:21 -0300

    versao inicial

---
+++ script.js
@@ -0,0 +1,8 @@
+const nome = "rayssa"
+const sobrenome = "jurca"
+const data_nascimento = "22/11/2006"
+let altura = 1.67
+let numero_sapato = 39
+let idade = 15
+let celular = "(18) 991072346"
+let cor_favorita = "marsala"
